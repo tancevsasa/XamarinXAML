b6f391d7156358a69fb66b9e0272fadb3e08fff4
Pavle Davitkovic <pajacar96@gmail.com>
2018-11-13 16:01:08 +0100

    colours add

---
+++ XamarinXAML/XamarinXAML/XamarinXAML/Resources/Colors.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinXAML.Resources
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Colors : ResourceDictionary
+    {
+        public Colors()
+        {
+            InitializeComponent();
+        }
+    }
+}
