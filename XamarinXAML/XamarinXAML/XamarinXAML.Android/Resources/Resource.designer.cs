ff7efba18bd199de6077ca2aba8570108fe66a7d
Pavle Davitkovic <pajacar96@gmail.com>
2018-11-15 12:54:48 +0100

    small fix

--- XamarinXAML/XamarinXAML/XamarinXAML.Android/Resources/Resource.designer.cs
+++ XamarinXAML/XamarinXAML/XamarinXAML.Android/Resources/Resource.designer.cs
@@ -7002,6 +7002,9 @@ namespace XamarinXAML.Droid
 			// aapt resource value: 0x7f0c0193
 			public const int MainTheme_Base = 2131493267;
 			
+			// aapt resource value: 0x7f0c0195
+			public const int MyTextAppearance_Material_Button = 2131493269;
+			
 			// aapt resource value: 0x7f0c002d
 			public const int Platform_AppCompat = 2131492909;
 			
